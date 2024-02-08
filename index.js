47ac54eca7fcdc57fd43b078d4b4624f440c2a69
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-02-08 20:18:46 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do meu sistema!", "darkgreen");
