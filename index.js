7589ba11d799f8ba9d6575c47f2b0cc8e5f5b2e7
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-02-08 21:11:15 -0300

    Acrescentando mais uma turboLogger

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
 turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do meu sistema!", "darkgreen");
+turboLogger("Opa! Gustavo dev fez uma atualização no código... Se liga!", "white");
 
 // git init -> inicia o git localmente (cria um repositório local que guarda o histórico de evolução do projeto).
 // git status -> mostra o status da branch atual.
