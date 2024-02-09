1a818fa8d6ed1a1ad247f30d21d4b2088b77c769
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-02-08 21:22:47 -0300

    Adicionando mais um Logger

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log("Estou usando o github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
 turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do meu sistema!", "darkgreen");
 turboLogger("Opa! Gustavo dev fez uma atualização no código... Se liga!", "white");
+
+turboLogger("Logger adicionado pela branch update_system_messages", "orange");
